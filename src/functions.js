0b8156bda872048561f74c55956e5d997a14c13d
Zulu0824 <sriharsha.patta@gmail.com>
2026-08-31 13:04:49 +0530

    add function caeser cipher

--- src/functions.js
+++ src/functions.js
@@ -20,3 +20,24 @@ export const calculator = {
     return a / b;
   },
 };
+
+function caesarCipher(str, key) {
+  return str
+    .split("")
+    .map((char) => {
+      const code = char.charCodeAt(0);
+
+      if (code >= 65 && code <= 90) {
+        return String.fromCharCode(((((code - 65 + key) % 26) + 26) % 26) + 65);
+      }
+
+      if (code >= 97 && code <= 122) {
+        return String.fromCharCode(((((code - 97 + key) % 26) + 26) % 26) + 97);
+      }
+
+      return char;
+    })
+    .join("");
+}
+
+console.log(caesarCipher("xyz", 3));
