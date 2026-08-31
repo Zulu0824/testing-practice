d62ca59dd1a76ec963c6e69c6aba4e59b98d2d65
Zulu0824 <sriharsha.patta@gmail.com>
2026-08-31 10:29:07 +0530

    add reverseString function

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,7 @@
 export function capitalize(str) {
   const capitalized = str.chatAt(0).toUpperCase() + str.slice(1);
 }
+
+export function reverseString(str) {
+  const reversed = str.split("").reverse().join("");
+}
